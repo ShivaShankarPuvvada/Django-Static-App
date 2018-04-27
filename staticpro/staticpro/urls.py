a30ad64a4b4bb7b904d96f5c62e2cbc642d40a97
Naga Sai Siva Sankar <om.ss.sankar@gmail.com>
2018-04-27 17:46:30 +0530

    Update urls.py

--- staticpro/staticpro/urls.py
+++ staticpro/staticpro/urls.py
@@ -20,4 +20,4 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.HomeView.as_view(),name='home')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
