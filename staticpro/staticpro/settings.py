20f6322813eccd9626305c10a7d63c815570933c
Naga Sai Siva Sankar <om.ss.sankar@gmail.com>
2018-05-02 12:35:21 +0530

    Update settings.py

--- staticpro/staticpro/settings.py
+++ staticpro/staticpro/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = "/home/senkar/senkar.pythonanywhere.com/staticpro/static"
+STATIC_ROOT = "/home/senkar/static"
 STATICFILES_DIRS = [
     '/static/',
 ]
